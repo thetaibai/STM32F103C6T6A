b85afd8e6ec19271b547186528cc0dbab908a6d7
LHL <a907764729@live.com>
2023-04-03 23:23:51 +0800

    提交文件目录

---
+++ 1.Project/USART2/Code/USART2/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'USART2' 
+ * Target:  'USART2' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
